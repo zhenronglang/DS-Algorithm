e91dee779fe97620e5f08f940df402f09f8ff551
zhenronglang <davidlang0832@gmail.com>
2022-11-20 12:41:10 +0100

    unique

---
+++ CUDA/Benchmarks/unique/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bas
+for((th=64; th<4097; th*=2)) 
+do
+	for ((co=8; co<th*4 && co<512; co*=2)) 
+	do
+		for i in $(seq 0 0.1 1) 
+		do
+			make clean
+			nvcc -O3 -arch=sm_52 unique.cu -I/usr/local/cuda/include -lm -o unique -D COARSENING=$co -D THREADS=$th -D INT  	
+
+			#Time=$(./streamcompaction 0 $i 8000000 | grep Throughput | awk '{print $4}')
+			Throughput=$(./unique 0 $i 512000000 | grep Throughput | awk '{print $8}')
+   			echo "$th, $co, $i, $Throughput"
+			echo "$th, $co, $i, $Throughput" >> uniquenonatomic.csv
+		done
+		echo " " >> uniquenonatomic.csv
+	done
+done
