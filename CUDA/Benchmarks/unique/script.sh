7052aa91de6fc56fd85c771a54a45b3c72b28726
zhenronglang <davidlang0832@gmail.com>
2022-11-20 14:11:33 +0100

    uniquenonatomic

--- CUDA/Benchmarks/unique/script.sh
+++ CUDA/Benchmarks/unique/script.sh
@@ -6,13 +6,13 @@ do
 		for i in $(seq 0 0.1 1) 
 		do
 			make clean
-			nvcc -O3 -arch=sm_52 unique.cu -I/usr/local/cuda/include -lm -o unique -D COARSENING=$co -D THREADS=$th -D INT  	
+			nvcc -O3 -arch=sm_52 unique.cu -I/usr/local/cuda/include -lm -o unique -D atom -D COARSENING=$co -D THREADS=$th -D INT  	
 
 			#Time=$(./streamcompaction 0 $i 8000000 | grep Throughput | awk '{print $4}')
 			Throughput=$(./unique 0 $i 64000000 | grep Throughput | awk '{print $8}')
    			echo "$th, $co, $i, $Throughput"
-			echo "$th, $co, $i, $Throughput" >> uniquenonatomic.csv
+			echo "$th, $co, $i, $Throughput" >> uniqueatomic.csv
 		done
-		echo " " >> uniquenonatomic.csv
+		echo " " >> uniqueatomic.csv
 	done
 done
